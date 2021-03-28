bb47aa880225e4e86c96392a518ab5405dfbaaec
[1000i100] Millicent Billette <git@1000i100.fr>
2021-03-28 20:52:11 +0200

    #10 #11 #14 #15 #18

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ var globRun = require('../');
 describe('glob-run node module', function () {
 	it('must list commands per pattern', function () {
 		var commands = globRun('browserify ' + __dirname + '/fixtures/*.js -o bundle.js');
-		assert.deepEqual(commands, [
+		assert.deepStrictEqual(commands, [
 			'browserify ' + __dirname + '/fixtures/a.js -o bundle.js',
 			'browserify ' + __dirname + '/fixtures/b.js -o bundle.js'
 		]);
@@ -15,7 +15,7 @@ describe('glob-run node module', function () {
 
 	it('must list commands per pattern, with matching file name', function () {
 		var commands = globRun('browserify ' + __dirname + '/fixtures/*.js -o *.js');
-		assert.deepEqual(commands, [
+		assert.deepStrictEqual(commands, [
 			'browserify ' + __dirname + '/fixtures/a.js -o a.js',
 			'browserify ' + __dirname + '/fixtures/b.js -o b.js'
 		]);
